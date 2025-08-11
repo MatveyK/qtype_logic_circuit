be176a39dae7331b334e17945a60ca1ddc356ac1
Matvey Khokhlov <matvey.khokhlov@epfl.ch>
2025-08-11 17:29:52 +0200

    Translate the plugin strings into french

--- lang/en/qtype_logiccircuit.php
+++ lang/en/qtype_logiccircuit.php
@@ -1,16 +1,13 @@
 <?php
 
 $string['pluginname'] = 'Logic Circuit Editor';
-$string['pluginname_help'] = 'Put delimiters around words considered correct, e.g. The cat [sat] on the mat. At runtime the user can select any of the words apart from the introduction text.';
-$string['pluginname_link'] = 'Logic_question_type';
+$string['pluginname_help'] = 'Create Logic Circuit exercises.';
+$string['pluginname_link'] = 'LogicCircuit_question_type';
 $string['pluginnameadding'] = 'Adding a logic circuit question';
 $string['pluginnameediting'] = 'Editing a logic circuit question';
-$string['pluginnamesummary'] = '';
-$string['correctanswer'] = 'Correct answer';
+$string['pluginnamesummary'] = 'Design and create logic circuit questions.';
 $string['initialstate'] = 'Initial state';
 $string['initialstate_help'] = 'Set the initial state of the circuit logic editor in JSON5 form. The initial state should include the tests against which the student\'s answer will be checked.';
-$string['endstate'] = 'Correct state';
-$string['endstate_help'] = 'Set the final correct state of the circuit logic editor in JSON5 form. The tests will be executed against this state.';
 $string['answer_incomplete'] = 'The answer is not complete.';
 $string['privacy:null_reason'] = 'The Logic Circuit question type does not effect or store any data itself.';
 $string['initial_state_empty'] = 'The initial state is empty';
---
+++ lang/fr/qtype_logiccircuit.php
@@ -0,0 +1,14 @@
+<?php
+
+$string['pluginname'] = 'Éditeur de circuits logiques';
+$string['pluginname_help'] = 'Créez des exercices de circuits logiques.';
+$string['pluginname_link'] = 'LogicCircuit_question_type';
+$string['pluginnameadding'] = 'Ajout d\'une question de circuit logique';
+$string['pluginnameediting'] = 'Édition d\'une question de circuit logique';
+$string['pluginnamesummary'] = 'Concevoir et créer des questions de circuits logiques.';
+$string['initialstate'] = 'État initial';
+$string['initialstate_help'] = 'Définir l\'état initial de l\'éditeur de circuits logiques sous forme de JSON5. L\'état initial doit inclure les tests contre lesquels la réponse de l\'étudiant sera vérifiée.';
+$string['answer_incomplete'] = 'La réponse est incomplète.';
+$string['privacy:null_reason'] = 'Le type de question Circuit logique n\'a aucun impact ni ne stocke de données.';
+$string['initial_state_empty'] = 'L\'état initial est vide';
+$string['not_valid_json'] = 'JSON5 non valide';
